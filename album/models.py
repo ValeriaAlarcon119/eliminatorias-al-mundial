78d3f3c274102eba2b3869450fa51331e168e915
Valeria Alarcon <valerialarcon119@gmail.com>
2023-10-25 19:12:05 -0500

    Actualizacion

--- album/models.py
+++ album/models.py
@@ -25,6 +25,9 @@ class Player(models.Model):
     weight = models.IntegerField()
     comment = models.CharField(max_length=200, blank=True)
     
+    def get_absolute_url(self):
+        return reverse('player-list')
+    
     def __str__(self):
         return self.first_name + " " + self.last_name
 
--- mundial/urls.py
+++ mundial/urls.py
@@ -24,5 +24,10 @@ urlpatterns = [
     path('selection/<int:pk>/detail/', views.SelectionDetailView.as_view(), name='selection-detail'),
     path('player/', views.PlayerListView.as_view(), name='player-list'),
     path('player/<int:pk>/detail/',views.PlayerDetailView.as_view(), name='player-detail'),
+    path('player/<int:pk>/update/',views.PlayerUpdate.as_view(),name='player-update'), 
+    #Create
+    path('player/create/', views.PlayerCreate.as_view(), name='player-create'),
+    #Delete
+    path('player/<int:pk>/delete/', views.PlayerDelete.as_view(), name='player-delete'),
 
 ]
